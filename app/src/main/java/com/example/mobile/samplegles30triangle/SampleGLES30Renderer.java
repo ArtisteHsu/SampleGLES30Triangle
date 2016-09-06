ba206b49b2584c8c7c434950eb9ec3827a002278
Artiste Hsu <chhsu@nvidia.com>
2016-09-06 09:10:44 +0800

    Initial commit.

---
+++ app/src/main/java/com/example/mobile/samplegles30triangle/SampleGLES30Renderer.java
@@ -0,0 +1,44 @@
+package com.example.mobile.samplegles30triangle;
+
+import android.content.res.Resources;
+import android.opengl.GLSurfaceView;
+
+import java.io.IOException;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by mobile on 2016/9/5.
+ */
+public class SampleGLES30Renderer implements GLSurfaceView.Renderer {
+    private Resources resources;
+    private SampleGLES30Triangle mTriangle;
+    int screenHeight = 0;
+    int screenWidth = 0;
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Set background color
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        mTriangle = new SampleGLES30Triangle();
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Redraw background color
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        mTriangle.Draw(screenWidth, screenHeight);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        screenHeight = height;
+        screenWidth = width;
+    }
+
+    public SampleGLES30Renderer(Resources res) {
+        resources = res;
+    }
+}
